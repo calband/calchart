2b9d10997ad6f298b78f7ba753e7c055abb8e0d9
Richard Powell <rmpowell77@me.com>
2010-04-22 04:48:32 +0000

    broke cc_shapes into their own file.  Trying to make source files smaller and easier to handle.
    git-svn-id: http://svn.code.sf.net/p/calchart/svn/trunk@170 eb3ee358-668b-4427-8ffb-f370b80720d0

--- src/main_ui.h
+++ src/main_ui.h
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "basic_ui.h"
 #include "show.h"
+#include "cc_shapes.h"
 
 #include <list>
 #include <vector>
@@ -109,132 +110,6 @@ enum CC_SELECT_TYPES
 };
 
 class MainFrame;
-
-class CC_shape
-{
-public:
-	CC_shape();
-	virtual ~CC_shape();
-
-	virtual void Draw(wxDC *dc, float x, float y) const = 0;
-	virtual void OnMove(const CC_coord& p, MainFrame *frame) = 0;
-};
-
-class CC_shape_1point: public CC_shape
-{
-public:
-	CC_shape_1point(const CC_coord& p);
-
-	virtual void OnMove(const CC_coord& p, MainFrame *frame);
-	void MoveOrigin(const CC_coord& p);
-	CC_coord GetOrigin() const;
-
-protected:
-	wxPoint origin;
-};
-
-class CC_shape_cross: public CC_shape_1point
-{
-public:
-	CC_shape_cross(const CC_coord& p, Coord width);
-
-	virtual void OnMove(const CC_coord& p, MainFrame *frame);
-	virtual void Draw(wxDC *dc, float x, float y) const;
-
-protected:
-	Coord cross_width;
-};
-
-class CC_shape_2point: public CC_shape_1point
-{
-public:
-	CC_shape_2point(const CC_coord& p);
-	CC_shape_2point(const CC_coord& p1, const CC_coord& p2);
-
-	virtual void OnMove(const CC_coord& p, MainFrame *frame);
-	void MovePoint(const CC_coord& p);
-	CC_coord GetPoint() const;
-
-protected:
-	wxPoint point;
-};
-
-class CC_shape_line: public CC_shape_2point
-{
-public:
-	CC_shape_line(const CC_coord& p);
-	CC_shape_line(const CC_coord& p1, const CC_coord& p2);
-
-	virtual void OnMove(const CC_coord& p, MainFrame *frame);
-	virtual void Draw(wxDC *dc, float x, float y) const;
-};
-
-class CC_shape_angline: public CC_shape_line
-{
-public:
-	CC_shape_angline(const CC_coord& p, const CC_coord& refvect);
-
-	virtual void OnMove(const CC_coord& p, MainFrame *frame);
-private:
-	CC_coord vect;
-	float mag;
-};
-
-class CC_shape_arc: public CC_shape_2point
-{
-public:
-	CC_shape_arc(const CC_coord& c, const CC_coord& p);
-	CC_shape_arc(const CC_coord& c, const CC_coord& p1, const CC_coord& p2);
-
-	virtual void OnMove(const CC_coord& p, MainFrame *frame);
-	virtual void Draw(wxDC *dc, float x, float y) const;
-
-	inline float GetAngle() const { return r-r0; }
-private:
-	float r,r0,d;
-};
-
-class CC_shape_rect: public CC_shape_2point
-{
-public:
-	CC_shape_rect(const CC_coord& p);
-	CC_shape_rect(const CC_coord& p1, const CC_coord& p2);
-
-	virtual void Draw(wxDC *dc, float x, float y) const;
-};
-
-class CC_lasso: public CC_shape
-{
-public:
-	CC_lasso(const CC_coord& p);
-	virtual ~CC_lasso();
-
-	virtual void OnMove(const CC_coord& p, MainFrame *frame);
-	void Clear();
-	void Start(const CC_coord& p);
-	void End();
-	void Append(const CC_coord& p);
-	bool Inside(const CC_coord& p) const;
-	virtual void Draw(wxDC *dc, float x, float y) const;
-	void Drag(const CC_coord& p);
-	inline const wxPoint *FirstPoint() const
-	{
-		return pntlist.empty() ? NULL : &pntlist.front();
-	}
-private:
-	bool CrossesLine(const wxPoint& start, const wxPoint& end,
-		const CC_coord& p) const;
-	typedef std::vector<wxPoint> PointList;
-	PointList pntlist;
-};
-
-class CC_poly: public CC_lasso
-{
-public:
-	CC_poly(const CC_coord& p);
-
-	virtual void OnMove(const CC_coord& p, MainFrame *frame);
-};
 
 class CC_WinNodeMain : public CC_WinNode
 {
