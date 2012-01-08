5fe4d92724c1019cf37756c4b08a6e488f728e00
Richard Powell <rmpowell77@me.com>
2012-01-08 22:47:13 +0000

    Adding much prettier Mac installer.
    git-svn-id: http://svn.code.sf.net/p/calchart/svn/trunk@331 eb3ee358-668b-4427-8ffb-f370b80720d0

--- build-macos/dmg_maker.sh
+++ build-macos/dmg_maker.sh
@@ -3,28 +3,30 @@
 PROJECT=CalChart
 VERSION=3.1.5
 APP=CalChart.app
+CONFIG=Debug
+WC_DMG=wc.dmg
+WC_DIR=wc
 
-echo Making a disk image...
-hdiutil create $PROJECT-$VERSION.dmg -volname $PROJECT-$VERSION -type UDIF -megabytes 50 -fs HFS+
+echo Creating a working copy for our DMG
+bunzip2 -k calchart_template.dmg.bz2
+cp calchart_template.dmg $WC_DMG
+rm calchart_template.dmg
 
 echo Mounting the disk image...
-hdiutil attach $PROJECT-$VERSION.dmg
-MYDEV=/Volumes/$PROJECT-$VERSION
-echo Device is $MYDEV
+mkdir -p $WC_DIR
+hdiutil attach "$WC_DMG" -noautoopen -quiet -mountpoint "$WC_DIR"
 
 echo Copying $PROJECT to the disk image...
-ditto --rsrc build/Debug/$APP /Volumes/$PROJECT-$VERSION/$APP
-touch /Volumes/$PROJECT-$VERSION/"DRAG CALCHART TO APPLICATIONS FOLDER"
-
-echo Unmountin the disk image...
-hdiutil detach $MYDEV
+rm -rf "$WC_DIR/$APP";
+ditto --rsrc "build/$CONFIG/$APP" "$WC_DIR/$APP"
+hdiutil detach $WC_DIR -quiet -force
+rm -rf $WC_DIR
 
 echo Compressing the disk image...
-hdiutil convert $PROJECT-$VERSION.dmg -format UDZO -o $PROJECT-$VERSION-compressed.dmg
+rm -f $PROJECT-$VERSION.dmg
+hdiutil convert $WC_DMG -format UDZO -imagekey zlib-level=9 -o $PROJECT-$VERSION.dmg
+rm -rf $WC_DMG
 
 echo Internet enbling the disk image...
-hdiutil internet-enable $PROJECT-$VERSION-compressed.dmg
+hdiutil internet-enable $PROJECT-$VERSION.dmg
 
-echo Renaming compressed image...
-rm -f $PROJECT-$VERSION.dmg
-mv $PROJECT-$VERSION-compressed.dmg $PROJECT-$VERSION.dmg
