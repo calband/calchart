299d140fc33a75a0cb8616d3c341e55a1e415509
Richard Powell <rmpowell77@me.com>
2013-09-03 05:54:40 +0000

    fixing up build for windows.
    git-svn-id: http://svn.code.sf.net/p/calchart/svn/trunk@498 eb3ee358-668b-4427-8ffb-f370b80720d0

--- src/cc_fileformat.h
+++ src/cc_fileformat.h
@@ -98,7 +98,7 @@ class CC_FileException : public std::runtime_error
 		uint8_t rawd[4];
 		put_big_long(rawd, nameID);
 		char s[128];
-		snprintf(s, sizeof(s), "Wrong ID read:  Read %c%c%c%c", rawd[0], rawd[1], rawd[2], rawd[3]);
+		_snprintf(s, sizeof(s), "Wrong ID read:  Read %c%c%c%c", rawd[0], rawd[1], rawd[2], rawd[3]);
 		return std::string(s);
 	}
 	
@@ -144,7 +144,7 @@ inline uint32_t ReadCheckIDandSize(T& stream, uint32_t inname)
 		uint8_t rawd[4];
 		put_big_long(rawd, inname);
 		char s[128];
-		snprintf(s, sizeof(s), "Wrong size %d for name %c%c%c%c", name, rawd[0], rawd[1], rawd[2], rawd[3]);
+		_snprintf(s, sizeof(s), "Wrong size %d for name %c%c%c%c", name, rawd[0], rawd[1], rawd[2], rawd[3]);
 		throw CC_FileException(s);
 	}
 	return ReadLong(stream);
