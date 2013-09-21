236ccfdf01c931b682bbba9c9b9c1b4ecd2e7ed7
Richard Powell <rmpowell77@me.com>
2013-09-21 19:16:33 +0000

    moving more things around, breaking wxWidget dependency of generated files.
    git-svn-id: http://svn.code.sf.net/p/calchart/svn/trunk@508 eb3ee358-668b-4427-8ffb-f370b80720d0

--- build-macos/calchart_cmd/main.cpp
+++ build-macos/calchart_cmd/main.cpp
@@ -6,20 +6,26 @@
 //
 //
 
-#include "cc_coord.h"
-#include "cc_continuity.h"
-#include "cc_point.h"
-#include "cc_text.h"
-#include "cc_sheet.h"
+#include "cc_show.h"
+#include "animate.h"
 
 #include <iostream>
 
+void Notify(const std::string& notice)
+{
+	std::cout<<notice<<"\n";
+}
+
+bool NotifyError(const std::vector<ErrorMarker>& error_markers, unsigned sheetnum, const std::string& message)
+{
+	return true;
+}
+
 int main(int argc, const char * argv[])
 {
-
 	// insert code here...
-	CC_continuity c("hi", 1);
-	CC_point p;
+	CC_show p;
+	Animation a(p, [](const std::string& notice) { std::cout<<notice<<"\n"; }, [](const std::vector<ErrorMarker>&, unsigned, const std::string&) { return true; });
 	std::cout << "Hello, World!\n";
     return 0;
 }
--- src/contgram.y
+++ src/contgram.y
@@ -24,7 +24,6 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <wx/wx.h>
 #include "cont.h"
 #include "parse.h"
 #include "animate.h"
