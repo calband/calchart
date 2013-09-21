ca8a623aecd8a8e2c89b06038ed4b0d5f7bb328d
Richard Powell <rmpowell77@me.com>
2013-09-21 18:20:26 +0000

    Adding a commandline app as a way to build independent from wxWidgets, do testing. Moving animate draw to cc_drawcommand.
    git-svn-id: http://svn.code.sf.net/p/calchart/svn/trunk@506 eb3ee358-668b-4427-8ffb-f370b80720d0

---
+++ build-macos/calchart_cmd/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  calchart_cmd
+//
+//  Created by Richard Powell on 9/20/13.
+//
+//
+
+#include "cc_coord.h"
+#include "cc_continuity.h"
+#include "cc_point.h"
+#include "cc_text.h"
+#include "cc_sheet.h"
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	CC_continuity c("hi", 1);
+	CC_point p;
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
