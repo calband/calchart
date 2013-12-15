9cd0596790a360103360c637143cfbf6a700f6f3
Richard Powell <rmpowell77@me.com>
2013-12-15 02:40:22 +0000

    Modified to have a check function as well as a print function.
    git-svn-id: http://svn.code.sf.net/p/calchart/svn/trunk@512 eb3ee358-668b-4427-8ffb-f370b80720d0

--- build-macos/calchart_cmd/main.cpp
+++ build-macos/calchart_cmd/main.cpp
@@ -7,11 +7,13 @@
 //
 
 #include "cc_show.h"
+#include "cc_sheet.h"
 #include "animate.h"
 
 #include <iostream>
 #include <fstream>
 #include <iterator>
+#include <unistd.h>
 
 void Notify(const std::string& notice)
 {
@@ -23,36 +25,107 @@ bool NotifyError(const std::vector<ErrorMarker>& error_markers, unsigned sheetnu
 	return true;
 }
 
-int main(int argc, const char * argv[])
+void PrintShow(const char* show)
 {
-	// insert code here...
-	if (argc > 1)
+	std::ifstream input(show);
+	CC_show p(input);
+	Animation a(p, [](const std::string& notice) { std::cout<<notice<<"\n"; }, [](const std::vector<ErrorMarker>&, unsigned, const std::string& error) { std::cout<<"error"<<error<<"\n"; return true; });
+	a.GotoSheet(0);
+	auto currentInfo = a.GetCurrentInfo();
+	std::cout<<currentInfo.first<<"\n";
+	std::copy(currentInfo.second.begin(), currentInfo.second.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+	auto oldInfo = currentInfo;
+	while (a.NextBeat())
 	{
-		std::ifstream input(argv[1]);
-		CC_show p(input);
-		Animation a(p, [](const std::string& notice) { std::cout<<notice<<"\n"; }, [](const std::vector<ErrorMarker>&, unsigned, const std::string& error) { std::cout<<"error"<<error<<"\n"; return true; });
-		a.GotoSheet(0);
 		auto currentInfo = a.GetCurrentInfo();
-		std::cout<<currentInfo.first<<"\n";
-		std::copy(currentInfo.second.begin(), currentInfo.second.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-		auto oldInfo = currentInfo;
-		while (a.NextBeat())
+		if (currentInfo.first != oldInfo.first)
 		{
-			auto currentInfo = a.GetCurrentInfo();
-			if (currentInfo.first != oldInfo.first)
-			{
-				std::cout<<currentInfo.first<<"\n";
-			}
-			for (auto i = 0; i < currentInfo.second.size(); ++i)
-			{
-				if (i < oldInfo.second.size() && oldInfo.second.at(i) == currentInfo.second.at(i))
-				{
-					continue;
-				}
-				std::cout<<currentInfo.second.at(i)<<"\n";
-			}
-			oldInfo = currentInfo;
+			std::cout<<currentInfo.first<<"\n";
 		}
+		for (auto i = 0; i < currentInfo.second.size(); ++i)
+		{
+			if (i < oldInfo.second.size() && oldInfo.second.at(i) == currentInfo.second.at(i))
+			{
+				continue;
+			}
+			std::cout<<currentInfo.second.at(i)<<"\n";
+		}
+		oldInfo = currentInfo;
+	}
+}
+
+bool ContinuityCountDifferentThanSymbol(const char* show)
+{
+	std::ifstream input(show);
+	if (!input.is_open())
+		return false;
+	const CC_show p(input);
+	for (auto i = p.GetSheetBegin(); i != p.GetSheetEnd(); ++i)
+	{
+		auto points = i->GetPoints();
+		std::map<SYMBOL_TYPE, uint8_t> continity_for_symbol;
+		std::map<uint8_t, SYMBOL_TYPE> symbol_for_continuity;
+		for (auto& point : points)
+		{
+			// if we haven't seen this cont symbol yet, then save the continuity index
+			// else, check the index, and if it's different, then return true;
+			auto symbol = point.GetSymbol();
+			auto cont_index = point.GetContinuityIndex();
+			if (continity_for_symbol.count(symbol) == 0)
+			{
+				continity_for_symbol[symbol] = cont_index;
+			}
+			else
+			{
+				if (continity_for_symbol[symbol] != cont_index)
+				{
+					return true;
+				}
+			}
+			if (symbol_for_continuity.count(cont_index) == 0)
+			{
+				symbol_for_continuity[cont_index] = symbol;
+			}
+			else
+			{
+				if (symbol_for_continuity[cont_index] != symbol)
+				{
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+int main(int argc, char * argv[])
+{
+    opterr = 0;
+	int doPrint = 0;
+	int doCheck = 0;
+	int c = 0;
+	while ((c = getopt (argc, argv, "cp")) != -1)
+		switch (c)
+	{
+		case 'p':
+			doPrint = true;
+			break;
+		case 'c':
+			doCheck = true;
+			break;
+	}
+	while (optind < argc)
+	{
+		if (doPrint)
+		{
+			PrintShow(argv[optind]);
+		}
+		if (doCheck)
+		{
+			std::cout<<"ContinuityCountDifferentThanSymbol ? "<<ContinuityCountDifferentThanSymbol(argv[1])<<"\n";
+		}
+		++optind;
 	}
 	
     return 0;
