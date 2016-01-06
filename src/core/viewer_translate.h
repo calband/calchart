7c217c1bfe1c12a823628c6fc5d6d02887aea499
Kevin Durand <kmdurand@kevins-mbp-2.gateway.pace.com>
2016-01-05 18:47:07 -0800

    Added comments to viewer translations, explaining the differences between calchart and the viewer

--- src/core/viewer_translate.h
+++ src/core/viewer_translate.h
@@ -16,8 +16,58 @@
  */
 class ToOnlineViewer {
 public:
+    /*!
+     * @brief Translates a CalChart field x-coordinate
+     * to a CalChart Online Viewer field x-coordinate.
+     * @details The origin for CalChart coordinates is
+     * found at the southwest corner of the field;
+     * x-coordinates increase in the northern direction
+     * at a rate of one unit per step. CalChart Online
+     * Viewer coordinates originate at the center of
+     * the field, with x-coordinates increasing in the
+     * northern direction at a rate of one unit per step.
+     * @param coord A CalChart field x-coordinate.
+     * @return An Online Viewer field x-coordinate.
+     */
     static float xPosition(Coord coord);
+    
+    /*!
+     * @brief Translates a CalChart field y-coordinate
+     * to a CalChart Online Viewer field y-coordinate.
+     * @details The origin for CalChart coordinates is
+     * found at the southwest corner of the field;
+     * y-coordinates increase in the eastward direction
+     * at a rate of one unit per step. CalChart
+     * Online Viewer coordinates originate at
+     * the center of the field, with y-coordinates
+     * increasing in the eastward direction at a rate
+     * of one unit per step.
+     * @param coord A CalChart field y-coordinate.
+     * @return An Online Viewer field y-coordinate.
+     */
     static float yPosition(Coord coord);
+    
+    /*!
+     * @brief Translates a CalChart angle to a
+     * CalChart Online Viewer angle.
+     * @details Angles in CalChart are measured
+     * in degrees counterclockwise from the northern
+     * direction. CalChart Online Viewer angles are
+     * measured in degrees clockwise from the
+     * eastward direction.
+     * @param angle A CalChart angle.
+     * @return An Online Viewer angle.
+     */
     static float angle(float angle);
+    
+    /*!
+     * @brief Translates a CalChart enumerated
+     * symbol type to its equivalent string
+     * representation in the CalChart Online Viewer.
+     * @param symbol A CalChart enumerated symbol
+     * type.
+     * @return The Online Viewer string representation
+     * of the CalChart symbol type.
+     */
     static std::string symbolName(SYMBOL_TYPE symbol);
 };
