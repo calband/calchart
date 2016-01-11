7793d40588d8866bd1a9a0e9a6eb002ef068b6c4
Kevin Durand <kmdurand@kevins-mbp-2.gateway.pace.com>
2016-01-11 10:31:26 -0800

    Added doxygen configuration, and installed doxygen tools to virtual machine

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -2,10 +2,28 @@
 
 apt-get update
 
+#  Install clang-format
+#  This tool is used for formatting our code
+apt-get install -y clang-format-3.6
+
+#  Install bison
 apt-get install -y bison
+
+#  Install flex
 apt-get install -y flex
 
-sudo apt-get install -y clang-format-3.6
+#  Install doxygen
+#  This tool is used to generate html documention of our code
+apt-get install -y doxygen
+
+#  Install graphviz to draw graphs in documentation
+apt-get install -y graphviz
+
+# Install mscgen to allow us to use message sequence charts in our documentation
+apt-get install -y mscgen
+
+# Install dia to allow us to use diagrams in our documentation
+apt-get install -y dia
 
 #  Copy bash_profile to the home directory (/home/vagrant/), so that 
 #+ the script will run each time we ssh into the virtual machine
