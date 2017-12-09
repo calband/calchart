23a518c2291445630720658e12dc787243e51398
Richard Powell <rmpowell77@me.com>
2017-12-09 11:28:15 -0800

    Adding a test suite.

---
+++ build-macos/calchart_core_tests/cc_show_test.mm
@@ -0,0 +1,22 @@
+//
+//  cc_show_tests.m
+//  cc_show_tests
+//
+//  Created by Richard Powell on 12/9/17.
+//
+
+#import <XCTest/XCTest.h>
+#import "cc_show.h"
+
+@interface cc_show_tests : XCTestCase
+
+@end
+
+@implementation cc_show_tests
+
+- (void)test_default_equal {
+    auto empty_show = CC_show::Create_CC_show();
+    XCTAssert(empty_show);
+}
+
+@end
