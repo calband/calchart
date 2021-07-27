6d84e80f1ee8ddc9ffa85dee2874fd6c891a932d
Richard Powell <rmpowell77@me.com>
2021-07-27 10:44:19 -0700

    restructuring cmake.

---
+++ cmake/version.cmake
@@ -0,0 +1,24 @@
+# version.cmake
+
+execute_process(COMMAND git describe --tags
+                OUTPUT_VARIABLE GIT_VERS
+                ERROR_QUIET)
+# get rid of 'v'
+string(STRIP ${GIT_VERS} GIT_VERS)
+string(SUBSTRING ${GIT_VERS} 1 -1 GIT_VERS)
+string(FIND ${GIT_VERS} "." bound)
+string(SUBSTRING ${GIT_VERS} 0 ${bound} Found_MAJOR)
+string(SUBSTRING ${GIT_VERS} ${bound} -1 GIT_VERS)
+string(SUBSTRING ${GIT_VERS} 1 -1 GIT_VERS)
+string(FIND ${GIT_VERS} "." bound)
+string(SUBSTRING ${GIT_VERS} 0 ${bound} Found_MINOR)
+string(SUBSTRING ${GIT_VERS} ${bound} -1 GIT_VERS)
+string(SUBSTRING ${GIT_VERS} 1 -1 GIT_VERS)
+set (Found_PATCH ${GIT_VERS})
+
+set (CalChart_VERSION_MAJOR ${Found_MAJOR})
+set (CalChart_VERSION_MINOR ${Found_MINOR})
+set (CalChart_VERSION_PATCH ${Found_PATCH})
+set (CalChart_VERSION "${CalChart_VERSION_MAJOR}.${CalChart_VERSION_MINOR}.${CalChart_VERSION_PATCH}")
+
+
