ace852113d737ba1515e6478a3f4f5d09ab8add3
Richard Powell <rmpowell77@me.com>
2021-08-06 18:14:22 -0700

    Fixing up the issues with opening images.  Need to be able to know where the image data ends.

--- src/core/CalChartImage.cpp
+++ src/core/CalChartImage.cpp
@@ -24,59 +24,47 @@
 
 namespace CalChart {
 
-ImageData::ImageData(int left, int top, int scaled_width, int scaled_height, int image_width, int image_height, std::vector<unsigned char> const& data, std::vector<unsigned char> const& alpha)
-    : left(left)
-    , top(top)
-    , scaled_width(scaled_width)
-    , scaled_height(scaled_height)
-    , image_width(image_width)
-    , image_height(image_height)
-    , data(data)
-    , alpha(alpha)
+std::pair<ImageData, uint8_t const*> CreateImageData(uint8_t const* d)
 {
-}
-
-// we don't have an end pointer, we just consum until we've finished.
-ImageData::ImageData(uint8_t const* d)
-{
-    left = get_big_long(d);
+    auto left = static_cast<int>(get_big_long(d));
     d += 4;
-    top = get_big_long(d);
+    auto top = static_cast<int>(get_big_long(d));
     d += 4;
-    scaled_width = get_big_long(d);
+    auto scaled_width = static_cast<int>(get_big_long(d));
     d += 4;
-    scaled_height = get_big_long(d);
+    auto scaled_height = static_cast<int>(get_big_long(d));
     d += 4;
-    image_width = get_big_long(d);
+    auto image_width = static_cast<int>(get_big_long(d));
     d += 4;
-    image_height = get_big_long(d);
+    auto image_height = static_cast<int>(get_big_long(d));
     d += 4;
     auto data_size = get_big_long(d);
     d += 4;
-    data.assign(d, d + data_size);
+    auto data = std::vector<unsigned char>(d, d + data_size);
     d += data_size;
     auto alpha_size = get_big_long(d);
     d += 4;
-    alpha.assign(d, d + alpha_size);
+    auto alpha = std::vector<unsigned char>(d, d + alpha_size);
     scaled_width = (scaled_width == 0) ? image_width : scaled_width;
     scaled_height = (scaled_height == 0) ? image_height : scaled_height;
+    return { ImageData{ left, top, scaled_width, scaled_height, image_width, image_height, data, alpha }, d };
 }
 
-std::vector<uint8_t> ImageData::Serialize() const
+std::vector<uint8_t> Serialize(ImageData const& image)
 {
     std::vector<uint8_t> result;
-    Parser::Append(result, uint32_t(left));
-    Parser::Append(result, uint32_t(top));
-    Parser::Append(result, uint32_t(scaled_width));
-    Parser::Append(result, uint32_t(scaled_height));
-    Parser::Append(result, uint32_t(image_width));
-    Parser::Append(result, uint32_t(image_height));
+    Parser::Append(result, uint32_t(image.left));
+    Parser::Append(result, uint32_t(image.top));
+    Parser::Append(result, uint32_t(image.scaled_width));
+    Parser::Append(result, uint32_t(image.scaled_height));
+    Parser::Append(result, uint32_t(image.image_width));
+    Parser::Append(result, uint32_t(image.image_height));
     // we know data size, but let's put it in anyways
-    Parser::Append(result, uint32_t(data.size()));
-    Parser::Append(result, data);
+    Parser::Append(result, uint32_t(image.data.size()));
+    Parser::Append(result, image.data);
     // alpha could be zero
-    Parser::Append(result, uint32_t(alpha.size()));
-    Parser::Append(result, alpha);
+    Parser::Append(result, uint32_t(image.alpha.size()));
+    Parser::Append(result, image.alpha);
     return result;
 }
 }
